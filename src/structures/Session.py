ed23f7088cdf3d2ae2ea6a71764f0d7800fb18f4
Greensky <david.hesliere@gmail.com>
2024-01-14 21:27:26 +0100

    feat: :sparkles: Add options in Session

--- src/structures/Session.py
+++ src/structures/Session.py
@@ -5,11 +5,14 @@ from Student import Student
 class Session():
     __teachers: typing.List[Teacher]
     __students: typing.List[Student]
+    __options: typing.List[str]
 
     def __init__(self) -> typing.NoReturn:
         self.__teachers = []
         self.__students = []
+        self.__options = []
     
+    # Getters
     @property
     def students(self) -> typing.List[Student]:
         return self.__students
@@ -18,9 +21,18 @@ class Session():
     def teachers(self) -> typing.List[Teacher]:
         return self.__teachers
 
-    def appendTeacher(self, teacher: Teacher) -> typing.Self:
+    @property
+    def options(self) -> typing.List[str]:
+        return self.__options
+
+    # Methods
+
+    def appendTeacher(self, teacher: Teacher):
         self.__teachers.append(teacher)
         return self
-    def appendStudents(self, student: Student) -> typing.Self:
+    def appendStudents(self, student: Student):
         self.__students.append(student)
+        return self
+    def addOption(self, option: str):
+        self.__options.append(option)
         return self
